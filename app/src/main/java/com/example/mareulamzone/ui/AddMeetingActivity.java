455978566c17c15be501566d526a4b5a489884a8
tqtmmpas7 <elomouda@hotmail.fr>
2021-09-27 17:05:40 +0200

    commit 27/09 | liste de participants good

--- app/src/main/java/com/example/mareulamzone/ui/AddMeetingActivity.java
+++ app/src/main/java/com/example/mareulamzone/ui/AddMeetingActivity.java
@@ -1,9 +1,5 @@
 package com.example.mareulamzone.ui;
 
-import static com.example.mareulamzone.service.LamzoneGenerator.USERS;
-import static com.google.android.material.internal.ContextUtils.getActivity;
-
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,9 +10,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mareulamzone.R;
@@ -36,13 +30,14 @@ public class AddMeetingActivity extends AppCompatActivity {
     private EditText etSubjectMeeting;
     private TimePicker tpEvent;
     private Spinner spMeetingDuration;
-    private EditText etAddUserToMeeting;
+    private Spinner spAddUserToMeeting;
     private Button btnAddUser;
     private RecyclerView rvParticipant;
     private Button btnAddMeeting;
 
     MeetingApiService mMeetingApiService;
     MeetingRoom meetingRoomChoose;
+    User userToAdd;
 
     Date submitDate;
     ArrayList<User> listParticipant = new ArrayList<>();
@@ -84,13 +79,26 @@ public class AddMeetingActivity extends AppCompatActivity {
 
     private void initAddUserToMeeting() {
 
-        
+        List<String> listEmail = mMeetingApiService.getAllEmails();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, listEmail);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spAddUserToMeeting.setAdapter(adapter);
+        spAddUserToMeeting.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+                userToAdd = mMeetingApiService.getUsers().get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         btnAddUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
 
 
             }
@@ -98,84 +106,83 @@ public class AddMeetingActivity extends AppCompatActivity {
     }
 
 
-
     private void initDatePicker() {
 
-                tpEvent.setIs24HourView(true);
-                final Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(System.currentTimeMillis());
-                dpEvent.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
-                    @Override
-                    public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
-                        year1 = year;
-                        month1 = month;
-                        dayOfMonth1 = dayOfMonth;
-                        if (hourOfDay1 != -1) {
-                            submitDate = getDate(year1, month1, dayOfMonth1, hourOfDay1, minute1);
-                        }
-                    }
-                });
-                tpEvent.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-                    @Override
-                    public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-                        hourOfDay1 = hourOfDay;
-                        minute1 = minute;
-                        if (year1 != -1) {
-                            submitDate = getDate(year1, month1, dayOfMonth1, hourOfDay1, minute1);
-                        }
-                    }
-                });
-            }
-
-            private Date getDate(int year, int month, int dayOfMonth, int hourOfDay, int minute) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.set(year, month, dayOfMonth, hourOfDay, minute);
-                dateInMillis = calendar.getTimeInMillis();
-                return new Date(dateInMillis);
-            }
-
-            private void initSpinnerRoom() {
-
-                List<String> list = new ArrayList<>();
-                for (MeetingRoom room : mMeetingApiService.getMeetingRooms()) {
-                    list.add(room.getName());
+        tpEvent.setIs24HourView(true);
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        dpEvent.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                year1 = year;
+                month1 = month;
+                dayOfMonth1 = dayOfMonth;
+                if (hourOfDay1 != -1) {
+                    submitDate = getDate(year1, month1, dayOfMonth1, hourOfDay1, minute1);
                 }
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, list);
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spMeetingRoom.setAdapter(adapter);
-                spMeetingRoom.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        meetingRoomChoose = mMeetingApiService.getMeetingRooms().get(position);
-                    }
-
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parent) {
-
-                    }
-                });
             }
-
-            private boolean checkUserEntries() {
-
-                //check spinner meetingroom
-                //check edittext subject
-                //check timepicker
-                //check spinner duration
-                //check email user to meeting
-
-                return false;
+        });
+        tpEvent.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                hourOfDay1 = hourOfDay;
+                minute1 = minute;
+                if (year1 != -1) {
+                    submitDate = getDate(year1, month1, dayOfMonth1, hourOfDay1, minute1);
+                }
             }
+        });
+    }
 
-            private void initView() {
-                spMeetingRoom = findViewById(R.id.sp_meeting_room);
-                etSubjectMeeting = findViewById(R.id.et_subject_meeting);
-                tpEvent = findViewById(R.id.tp_event);
-                spMeetingDuration = findViewById(R.id.sp_meeting_duration);
-                etAddUserToMeeting = findViewById(R.id.et_add_user_to_meeting);
-                btnAddUser = findViewById(R.id.btn_add_user);
-                rvParticipant = findViewById(R.id.rv_participant);
-                btnAddMeeting = findViewById(R.id.btn_add_meeting);
-                dpEvent = findViewById(R.id.dp_event);
-            }
+    private Date getDate(int year, int month, int dayOfMonth, int hourOfDay, int minute) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, dayOfMonth, hourOfDay, minute);
+        dateInMillis = calendar.getTimeInMillis();
+        return new Date(dateInMillis);
+    }
+
+    private void initSpinnerRoom() {
+
+        List<String> list = new ArrayList<>();
+        for (MeetingRoom room : mMeetingApiService.getMeetingRooms()) {
+            list.add(room.getName());
         }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spMeetingRoom.setAdapter(adapter);
+        spMeetingRoom.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                meetingRoomChoose = mMeetingApiService.getMeetingRooms().get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private boolean checkUserEntries() {
+
+        //check spinner meetingroom
+        //check edittext subject
+        //check timepicker
+        //check spinner duration
+        //check email user to meeting
+
+        return false;
+    }
+
+    private void initView() {
+        spMeetingRoom = findViewById(R.id.sp_meeting_room);
+        etSubjectMeeting = findViewById(R.id.et_subject_meeting);
+        tpEvent = findViewById(R.id.tp_event);
+        spMeetingDuration = findViewById(R.id.sp_meeting_duration);
+        spAddUserToMeeting = findViewById(R.id.sp_add_user_to_meeting);
+        btnAddUser = findViewById(R.id.btn_add_user);
+        rvParticipant = findViewById(R.id.rv_participant);
+        btnAddMeeting = findViewById(R.id.btn_add_meeting);
+        dpEvent = findViewById(R.id.dp_event);
+    }
+}
